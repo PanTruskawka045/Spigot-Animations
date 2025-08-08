fdb91d64e62fea12d0bc56005046dc2d2830ceba
PanTruskawka045 <pan.truskawka045@gmail.com>
2025-08-08 23:02:21 +0200

    Add cubic and sine easing functions to EaseFunctions

--- src/main/java/me/pan_truskawka045/effects3d/animations/EaseFunctions.java
+++ src/main/java/me/pan_truskawka045/effects3d/animations/EaseFunctions.java
@@ -21,6 +21,36 @@ public class EaseFunctions {
      * <pre>f(x) = x(2 - x)</pre>
      */
     public static final EaseFunction EASE_OUT = f -> f * (2 - f);
+    /**
+     * Cubic ease-in function
+     * Defined by:
+     * <pre>f(x) = x^3</pre>
+     */
+    public static final EaseFunction CUBIC_IN = f -> f * f * f;
+
+    /**
+     * Cubic ease-out function
+     * Defined by:
+     * <pre>f(x) = (--f) * f * f + 1</pre>
+     */
+    public static final EaseFunction CUBIC_OUT = f -> {
+        float t = f - 1;
+        return t * t * t + 1;
+    };
+
+    /**
+     * Sine ease-in function
+     * Defined by:
+     * <pre>f(x) = 1 - cos((x * PI) / 2)</pre>
+     */
+    public static final EaseFunction SINE_IN = f -> (float)(1 - Math.cos((f * Math.PI) / 2));
+
+    /**
+     * Sine ease-out function
+     * Defined by:
+     * <pre>f(x) = sin((x * PI) / 2)</pre>
+     */
+    public static final EaseFunction SINE_OUT = f -> (float)Math.sin((f * Math.PI) / 2);
 
 
 }
